97d285dd8cea528f66c49ff4ac737fa0173574a6
Richard King <richrdkng@gmail.com>
2020-05-10 19:02:05 +0200

    feat: add initial Stream handling
    Also emphasize toString encoding.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ const replacer = require('./__internal/replacer')
 
 module.exports = data =>
   tap(file => {
+    if (file.isStream()) {
+      throw new Error('@zengulp/intradoc-replacer: \'file\' must be a Buffer, Streaming not supported')
+    }
+
     file.contents = Buffer.from(
       replacer(
-        file.contents.toString(),
+        file.contents.toString('utf8'),
         data
       )
     )
